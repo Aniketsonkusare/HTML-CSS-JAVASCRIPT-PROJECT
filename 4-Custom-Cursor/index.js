8ad3eee2c84a16902ad7c00d0a01c3ce92213637
Aniketsonkusare <aniketsonkusare70@gmail.com>
2024-03-13 21:57:21 +0530

    Custom-Cursor

---
+++ 4-Custom-Cursor/index.js
@@ -0,0 +1,17 @@
+let elem = document.querySelectorAll('.elem')
+
+elem.forEach((val) => {
+    console.log(val.childNodes);
+
+    val.addEventListener('mouseenter',function () {
+        val.childNodes[3].style.opacity = 1
+    })
+
+    val.addEventListener('mouseleave',function () {
+        val.childNodes[3].style.opacity = 0
+    })
+
+    val.addEventListener('mousemove',function (params) {
+        val.childNodes[3].style.left = params.x + "px"
+    })
+})
